f74c08425be6aa68e8f2a41912389cc35857041d
Danny van der Zalm <danny@connectholland.nl>
2016-12-19 15:34:41 +0100

    Initial commit

---
+++ checkPackages.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Process\Process;
+
+putenv('COMPOSER_HOME='.__DIR__.'/vendor/bin/composer');
+chdir(__DIR__);
+
+$process = new Process('composer outdated');
+$process->run();
+$outputSplitted = explode("\n",$process->getOutput());
+
+if (is_array($outputSplitted)) {
+    $output = '<table>';
+    foreach ($outputSplitted as $package) {
+        if (!empty($package)) {
+            $packageArray = explode(' ',$package);
+            $output .= '<tr>';
+            $i = 0;
+            foreach ($packageArray as $packageInfo) {
+                if (!empty($packageInfo) && $i < 3) {
+                    $output .= '<td class="col'.$i.'">'.$packageInfo.'</td>';
+                    $i++;
+                }
+            }
+            $ouput .= '</tr>';
+        }
+    }
+    $output .= '</table>';
+
+    echo $output;
+}
